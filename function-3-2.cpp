901534d447cdde1837c902d8a3ffa6e1ad81066a
An Phan <tqtthuyan71@gmail.com>
2024-03-01 11:19:30 +1030

    3-2

---
+++ function-3-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int median_array(int array[], int n)
+{
+    if ((n < 1) && (n % 2 ==0))
+    {
+        return false;
+    }
+
+    sort (array, array + n);
+    int median = array[n/2];
+    
+    return median;
+}

93e2663bbc7edf1e632cfb6cb777c58c1c4a1a43
An Phan <tqtthuyan71@gmail.com>
2024-03-01 15:17:18 +1030

    3-5

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -6,14 +6,15 @@ using namespace std;
 
 double sum_even(double array[], int n)
 {
+    double sum;
     if(n < 1)
     {
         return 0;
     }
-int sum = 0;
+
     for( int i = 1; i < n; i++)
     {
-        if ( i%2 == 0)
+        if ( i % 2 == 0)
         {
              sum += array[i];
         }

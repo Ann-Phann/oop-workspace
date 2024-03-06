3896404493396426b86e6f589baf39ddf73f9192
An Phan <tqtthuyan71@gmail.com>
2024-03-06 21:21:11 +1030

    ez2

---
+++ main-1-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int sum_diagonal(int array[4][4]);
+
+int main (void)
+{
+    int array[4][4]  = {
+        {1,2,3,4},
+        {4,5,6,7},
+        {1,2,3,4},
+        {1,2,3,4}
+    };
+    cout << sum_diagonal(array);
+}
---
+++ main-1-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int is_identity(int array[10][10]);
+
+int main(void)
+{
+    int array[10][10] = {
+        {1,0,0,0,0,0,0,0,0,0},
+        {0,1,0,0,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0,0,0,0},
+        {0,0,0,1,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0,0,0,0},
+        {0,0,0,0,0,1,0,0,0,0},
+        {0,0,0,0,0,0,1,0,0,0},
+        {0,0,0,0,0,0,0,1,0,0},
+        {0,0,0,0,0,0,0,0,1,0},
+        {0,0,0,0,0,0,0,0,0,1},
+    };
+
+    if (is_identity(array) == 0)
+    {
+        cout << "This is identity matrix";
+    }
+    else {
+        cout << "This is not identity matrix";
+    }
+}

10ed4503f6e7f4195db5c109f6c196435b992af5
An Phan <tqtthuyan71@gmail.com>
2024-03-14 10:32:14 +1030

    q1q2

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 double arrayMin(double* array, int size)
 {
-    int min = array[0];
+    double min = array[0];
     for (int i = 1; i < size; i++)
     {
         if (min > array[i])

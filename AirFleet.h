f0d02914d3c114da77669504df41138d81d5665a
An Phan <tqtthuyan71@gmail.com>
2024-05-02 11:35:38 +0930

    main22

---
+++ AirFleet.h
@@ -0,0 +1,7 @@
+#ifndef ""
+AirFleet()                          
+/* creates an air fleet containing pointers to 5 AirCraft objects as an array; 
+*/
+
+AirCraft **fleet;
+AirCraft **get_fleet()      // returns the array of pointers to the five aircraft objects

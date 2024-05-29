21f025c1eba02f64138454adc044d0ce546e1029
An Phan <tqtthuyan71@gmail.com>
2024-05-30 00:07:46 +0930

    delete all

--- main-2-1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "AirCraft.h"
-#include "Helicopter.h"
-using namespace std;
-
-int main()
-{
-    return 0;
-}
--- main-2-2.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-#include "Airplane.h"
-
-int main(void)
-{}

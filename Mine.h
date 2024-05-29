23979ac849acad311d9af746d58bcdeb2231c422
Ann-Phann <151800713+Ann-Phann@users.noreply.github.com>
2024-05-29 23:22:44 +0930

    Update Mine.h

--- Mine.h
+++ Mine.h
@@ -5,18 +5,30 @@
 #include "Explosion.h"
 #include <iostream>
 
-class Mine : public GameEntity
-{
+/**
+ * @class Mine
+ * @brief Represents a mine entity in the game.
+ *        Inherits from GameEntity.
+ */
+class Mine : public GameEntity {
 public:
-    Mine(int x, int y) : GameEntity(x, y, 'M')
-    {}
-    
-    Explosion explode()
-    {
-        this->setType('X');
-        return Explosion(std::get<0>(position), std::get<1>(position));
-    }
+    /**
+     * @brief Constructor to initialize the Mine with a position.
+     * @param x The x-coordinate of the mine.
+     * @param y The y-coordinate of the mine.
+     */
+    Mine(int x, int y) : GameEntity(x, y, 'M') {}
 
+    /**
+     * @brief Creates an Explosion object at the mine's position.
+     *        Changes the mine's type to 'X' to indicate it has exploded.
+     * @return An Explosion object at the mine's current position.
+     */
+    Explosion explode() {
+        this->setType('X');
+        std::tuple<int, int> pos = getPos();
+        return Explosion(std::get<0>(pos), std::get<1>(pos));
+    }
 };
 
 #endif // MINE_H

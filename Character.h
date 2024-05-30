5ce5ad2188e8cff06d11dbf02bded61864a29b97
An Phan <tqtthuyan71@gmail.com>
2024-05-30 10:36:19 +0930

    q2 complete

---
+++ Character.h
@@ -0,0 +1,18 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include "Cell.h"
+
+class Character : public Cell {
+public:
+    Character(int x, int y) : Cell(x, y, 'C') {}
+
+    void move(int dx, int dy) {
+        std::tuple<int, int> pos = getPos();
+        int x = std::get<0>(pos);
+        int y = std::get<1>(pos);
+        setPos(x + dx, y + dy);    
+    }
+};
+
+#endif // CHARACTER_H

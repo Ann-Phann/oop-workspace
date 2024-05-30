bda8e2974dfdab3461e527a9ca758d414629742a
Ann-Phann <151800713+Ann-Phann@users.noreply.github.com>
2024-05-30 11:19:31 +0930

    Update Game.h

--- Game.h
+++ Game.h
@@ -3,49 +3,82 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
 #include <cstdlib>
 #include <ctime>
 #include "Cell.h"
+#include "Utils.h"
 #include "Character.h"
 #include "Trap.h"
-#include "Utils.h"
 
 class Game {
 private:
-    std::vector<Cell*> grid;
+    std::vector<Cell*> entities;
+    int gridWidth;
+    int gridHeight;
 
 public:
     Game() {
-        std::srand(std::time(0)); // Seed for random number generation
-
-        //or 
-        // std::srand(static_cast<unsigned int>(std::time(0)));
+        std::srand(static_cast<unsigned int>(std::time(0))); // Seed for random number generation
     }
 
-    std::vector<Cell*>& getGrid() {
-        return grid;
+    // Getter for entities
+    std::vector<Cell*>& get_entities() {
+        return entities;
     }
 
-    void initGame(int numCharacters, int numTraps, int gridWidth, int gridHeight) {
-        // Initialize characters at random positions
-        for (int i = 0; i < numCharacters; ++i) {
-            auto pos = Utils::generateRandomPos(gridWidth, gridHeight);
-            grid.push_back(new Character(std::get<0>(pos), std::get<1>(pos)));
+    // Setter for entities
+    void set_entities(const std::vector<Cell*>& new_entities) {
+        this->entities = new_entities;
+    }
+
+    // Initializes the game with specified numbers of characters and traps
+    std::vector<Cell*> initGame(int numCharacters, int numTraps, int gridWidth, int gridHeight) {
+        this->gridWidth = gridWidth;
+        this->gridHeight = gridHeight;
+
+        // Clear existing entities
+        entities.clear();
+
+        // Initialize characters with random positions
+        for (int i = 0; i < numCharacters; i++) {
+            auto position = Utils::generateRandomPos(gridWidth, gridHeight);
+            Cell* character = new Character(std::get<0>(position), std::get<1>(position));
+            entities.push_back(character);
         }
 
-        // Initialize traps at random positions
-        for (int i = 0; i < numTraps; ++i) {
-            auto pos = Utils::generateRandomPos(gridWidth, gridHeight);
-            grid.push_back(new Trap(std::get<0>(pos), std::get<1>(pos)));
+        // Initialize traps with random positions
+        for (int j = 0; j < numTraps; j++) {
+            auto position = Utils::generateRandomPos(gridWidth, gridHeight);
+            Cell* trap = new Trap(std::get<0>(position), std::get<1>(position));
+            entities.push_back(trap);
         }
+
+        return entities;
     }
 
-    void gameLoop(int maxIterations, double trapActivationDistance, int gridWidth, int gridHeight) {
-        for (int iteration = 0; iteration < maxIterations; ++iteration) {
-            for (auto& cell : grid) {
-                // Move characters
-                if (Character* character = dynamic_cast<Character*>(cell)) {
+    // Simulates the game
+    void gameLoop(int maxIterations, double trapActivationDistance) {
+        for (int iteration = 0; iteration < maxIterations; iteration++) {
+            for (int i = 0; i < static_cast<int>(entities.size()); i++) {
+                if (entities[i]->getType() == 'C') {
+                    // Move all characters
+                    Character* character = static_cast<Character*>(entities[i]);
                     character->move(1, 0);
+                    auto character_pos = character->getPos();
+
+                    // Check if any character is within a certain distance of a trap
+                    for (int j = 0; j < static_cast<int>(entities.size()); j++) {
+                        if (entities[j]->getType() == 'T') {
+                            Trap* trap = static_cast<Trap*>(entities[j]);
+                            auto trap_pos = trap->getPos();
+                            double distance = Utils::calculateDistance(character_pos, trap_pos);
+
+                            if (distance <= trapActivationDistance) {
+                                trap->apply(*character);
+                            }
+                        }
+                    }
 
                     // Check if the character has won the game
                     auto [x, y] = character->getPos();
@@ -53,24 +86,29 @@ public:
                         std::cout << "Character has won the game!" << std::endl;
                         return;
                     }
-
-                    // Check for nearby traps
-                    for (auto& otherCell : grid) {
-                        if (Trap* trap = dynamic_cast<Trap*>(otherCell)) {
-                            if (trap->isActive() && Utils::calculateDistance(character->getPos(), trap->getPos()) <= trapActivationDistance) {
-                                trap->apply(*character);
-                            }
-                        }
-                    }
                 }
+            }
+
+            // Check if all characters are destroyed
+            bool allCharactersDestroyed = std::all_of(entities.begin(), entities.end(), [](Cell* entity) {
+                if (entity->getType() == 'C') {
+                    Character* character = static_cast<Character*>(entity);
+                    return character->getType() == 'X'; // 'X' means the character is destroyed
+                }
+                return true;
+            });
+
+            if (allCharactersDestroyed) {
+                std::cout << "All characters are destroyed. Game over." << std::endl;
+                return;
             }
         }
         std::cout << "Maximum number of iterations reached. Game over." << std::endl;
     }
 
     ~Game() {
-        for (auto& cell : grid) {
-            delete cell;
+        for (auto entity : entities) {
+            delete entity;
         }
     }
 };

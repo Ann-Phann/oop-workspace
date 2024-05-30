d09bf5585cb9c03668537dc14d8090c9ec563dbd
Ann-Phann <151800713+Ann-Phann@users.noreply.github.com>
2024-05-30 10:18:23 +0930

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -1,1 +1,32 @@
+#ifndef CELL_H
+#define CELL_H
+
+#include <tuple>
+
+class Cell {
+private:
+    std::tuple<int, int> position;
+    char type;
+
+public:
+    Cell(int x, int y, char type) : position(x, y), type(type) {}
+
+    std::tuple<int, int> getPos() const {
+        return position;
+    }
+
+    char getType() const {
+        return type;
+    }
+
+    void setPos(int x, int y) {
+        position = std::make_tuple(x, y);
+    }
+
+    void setType(char type) {
+        this->type = type;
+    }
+};
+
+#endif // CELL_H
 

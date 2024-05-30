f8abcc368db32c70435fc73162f641829b3135c1
Ann-Phann <151800713+Ann-Phann@users.noreply.github.com>
2024-05-30 10:42:14 +0930

    Update Game.h

--- Game.h
+++ Game.h
@@ -10,7 +10,6 @@
 #include "Trap.h"
 #include "Utils.h"
 
-
 class Game {
 private:
     std::vector<Cell*> grid;
@@ -38,13 +37,39 @@ public:
         }
     }
 
+    void gameLoop(int maxIterations, double trapActivationDistance, int gridWidth, int gridHeight) {
+        for (int iteration = 0; iteration < maxIterations; ++iteration) {
+            for (auto& cell : grid) {
+                // Move characters
+                if (Character* character = dynamic_cast<Character*>(cell)) {
+                    character->move(1, 0);
 
+                    // Check if the character has won the game
+                    auto [x, y] = character->getPos();
+                    if (x >= gridWidth || y >= gridHeight || x < 0 || y < 0) {
+                        std::cout << "Character has won the game!" << std::endl;
+                        return;
+                    }
 
-~Game() {
+                    // Check for nearby traps
+                    for (auto& otherCell : grid) {
+                        if (Trap* trap = dynamic_cast<Trap*>(otherCell)) {
+                            if (trap->isActive() && Utils::calculateDistance(character->getPos(), trap->getPos()) <= trapActivationDistance) {
+                                trap->apply(*character);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        std::cout << "Maximum number of iterations reached. Game over." << std::endl;
+    }
+
+    ~Game() {
         for (auto& cell : grid) {
             delete cell;
         }
     }
-
+};
 
 #endif // GAME_H

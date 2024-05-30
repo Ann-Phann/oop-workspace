00dcddaa932e2f41e38e82ad34721a7934707f37
An Phan <tqtthuyan71@gmail.com>
2024-05-30 11:52:20 +0930

    again

--- Game.h
+++ Game.h
@@ -30,48 +30,102 @@ public:
         this->grid = new_entities;
     }
 
-    // Initializes the game with specified numbers of characters and traps
-    std::vector<Cell*> initGame(int numCharacters, int numTraps, int gridWidth, int gridHeight) {
+    // // Initializes the game with specified numbers of characters and traps
+    // std::vector<Cell*> initGame(int numCharacters, int numTraps, int gridWidth, int gridHeight) {
+    //     this->gridWidth = gridWidth;
+    //     this->gridHeight = gridHeight;
+
+    //     // Clear existing entities
+    //     grid.clear();
+
+    //     // Initialize characters with random positions
+    //     for (int i = 0; i < numCharacters; i++) {
+    //         std::tuple<int, int> position = Utils::generateRandomPos(gridWidth, gridHeight);
+    //         Cell* character = new Character(std::get<0>(position), std::get<1>(position));
+    //         grid.push_back(character);
+    //     }
+
+    //     // Initialize traps with random positions
+    //     for (int j = 0; j < numTraps; j++) {
+    //         std::tuple<int, int> position = Utils::generateRandomPos(gridWidth, gridHeight);
+    //         Cell* trap = new Trap(std::get<0>(position), std::get<1>(position));
+    //         grid.push_back(trap);
+    //     }
+
+    //     return grid;
+    // }
+
+    // // Simulates the game
+    // void gameLoop(int maxIterations, double trapActivationDistance) 
+    // {
+    //     for (int iteration = 0; iteration < maxIterations; iteration++) {
+    //         for (int i = 0; i < static_cast<int>(grid.size()); i++) {
+    //             if (grid[i]->getType() == 'C') {
+    //                 // Move all characters to the right
+    //                 Character* character = static_cast<Character*>(grid[i]);
+    //                 character->move(1, 0);
+    //                 std::tuple<int, int> character_pos = character->getPos();
+
+    //                 // Check if any character is within a certain distance of a trap
+    //                 for (int j = 0; j < static_cast<int>(grid.size()); j++) {
+    //                     if (grid[j]->getType() == 'T') {
+    //                         Trap* trap = static_cast<Trap*>(grid[j]);
+    //                         std::tuple<int, int> trap_pos = trap->getPos();
+    //                         double distance = Utils::calculateDistance(character_pos, trap_pos);
+
+    //                         if (distance <= trapActivationDistance) {
+    //                             trap->apply(*character);
+    //                         }
+    //                     }
+    //                 }
+
+    //                 // Check if the character has won the game
+    //                 int x = std::get<0>(character_pos);
+    //                 int y = std::get<1>(character_pos);
+    //                 if (x >= gridWidth || y >= gridHeight || x < 0 || y < 0) {
+    //                     std::cout << "Character has won the game!" << std::endl;
+    //                     return;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     std::cout << "Maximum number of iterations reached. Game over." << std::endl;
+    //     return;
+    // }
+
+std::vector<Cell*> initGame(int numCharacters, int numTraps, int gridWidth, int gridHeight) {
         this->gridWidth = gridWidth;
         this->gridHeight = gridHeight;
+        cells.clear();
 
-        // Clear existing entities
-        grid.clear();
-
-        // Initialize characters with random positions
         for (int i = 0; i < numCharacters; i++) {
             std::tuple<int, int> position = Utils::generateRandomPos(gridWidth, gridHeight);
             Cell* character = new Character(std::get<0>(position), std::get<1>(position));
-            grid.push_back(character);
+            cells.push_back(character);
         }
 
-        // Initialize traps with random positions
         for (int j = 0; j < numTraps; j++) {
             std::tuple<int, int> position = Utils::generateRandomPos(gridWidth, gridHeight);
             Cell* trap = new Trap(std::get<0>(position), std::get<1>(position));
-            grid.push_back(trap);
+            cells.push_back(trap);
         }
 
-        return grid;
+        return cells;
     }
 
-    // Simulates the game
-    void gameLoop(int maxIterations, double trapActivationDistance) 
-    {
+    void gameLoop(int maxIterations, double trapActivationDistance) {
         for (int iteration = 0; iteration < maxIterations; iteration++) {
-            for (int i = 0; i < static_cast<int>(grid.size()); i++) {
-                if (grid[i]->getType() == 'C') {
-                    // Move all characters to the right
-                    Character* character = static_cast<Character*>(grid[i]);
+            for (int i = 0; i < static_cast<int>(cells.size()); i++) {
+                if (cells[i]->getType() == 'C') {
+                    Character* character = static_cast<Character*>(cells[i]);
                     character->move(1, 0);
-                    std::tuple<int, int> character_pos = character->getPos();
+                    std::tuple<int, int> char_pos = character->getPos();
 
-                    // Check if any character is within a certain distance of a trap
-                    for (int j = 0; j < static_cast<int>(grid.size()); j++) {
-                        if (grid[j]->getType() == 'T') {
-                            Trap* trap = static_cast<Trap*>(grid[j]);
+                    for (int j = 0; j < static_cast<int>(cells.size()); j++) {
+                        if (cells[j]->getType() == 'T') {
+                            Trap* trap = static_cast<Trap*>(cells[j]);
                             std::tuple<int, int> trap_pos = trap->getPos();
-                            double distance = Utils::calculateDistance(character_pos, trap_pos);
+                            double distance = Utils::calculateDistance(char_pos, trap_pos);
 
                             if (distance <= trapActivationDistance) {
                                 trap->apply(*character);
@@ -79,10 +133,7 @@ public:
                         }
                     }
 
-                    // Check if the character has won the game
-                    int x = std::get<0>(character_pos);
-                    int y = std::get<1>(character_pos);
-                    if (x >= gridWidth || y >= gridHeight || x < 0 || y < 0) {
+                    if (std::get<0>(char_pos) >= gridWidth || std::get<1>(char_pos) >= gridHeight) {
                         std::cout << "Character has won the game!" << std::endl;
                         return;
                     }

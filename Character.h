6769cc63c2ca57c3719262880f7ef24ffdde9742
Ann-Phann <151800713+Ann-Phann@users.noreply.github.com>
2024-05-30 10:29:02 +0930

    Update Character.h

--- Character.h
+++ Character.h
@@ -1,1 +1,17 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include "Cell.h"
+
+class Character : public Cell {
+public:
+    Character(int x, int y) : Cell(x, y, 'C') {}
+
+    void move(int dx, int dy) {
+        auto [x, y] = getPos();
+        setPos(x + dx, y + dy);
+    }
+};
+
+#endif // CHARACTER_H
 

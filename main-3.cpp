d78ac2a0cf8315076824754d9bc54e52d8b19678
Ann-Phann <151800713+Ann-Phann@users.noreply.github.com>
2024-05-30 11:51:18 +0930

    Update main-3.cpp

--- main-3.cpp
+++ main-3.cpp
@@ -1,16 +1,22 @@
+#include <iostream>
 #include "Game.h"
+#include "Utils.h"
+#include "Character.h"
+#include "Trap.h"
 
 int main() {
     Game game;
-    int numCharacters = 2;
-    int numTraps = 3;
+
+    int numCharacters = 3;
+    int numTraps = 5;
     int gridWidth = 10;
     int gridHeight = 10;
+    game.initGame(numCharacters, numTraps, gridWidth, gridHeight);
+
     int maxIterations = 20;
     double trapActivationDistance = 1.5;
 
-    game.initGame(numCharacters, numTraps, gridWidth, gridHeight);
-    game.gameLoop(maxIterations, trapActivationDistance, gridWidth, gridHeight);
+    game.gameLoop(maxIterations, trapActivationDistance);
 
     return 0;
 }

3c0f97ee46102dddd4d2a88f71c0264f2ec12fd0
Ann-Phann <151800713+Ann-Phann@users.noreply.github.com>
2024-05-30 10:37:18 +0930

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,1 +1,33 @@
+#ifndef GAME_H
+#define GAME_H
 
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Cell.h"
+#include "Character.h"
+#include "Trap.h"
+#include "Utils.h"
+
+
+class Game {
+private:
+    std::vector<Cell*> grid;
+
+public:
+    Game() {
+        std::srand(std::time(0)); // Seed for random number generation
+    }
+
+    std::vector<Cell*>& getGrid() {
+        return grid;
+    }
+    ~Game() {
+        for (auto& cell : grid) {
+            delete cell;
+        }
+    }
+
+
+#endif // GAME_H
